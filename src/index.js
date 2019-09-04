4e4c023b33423f191b9d4dca491c0ba981e22d8b
Aizhan87 <moonsoul87@hotmail.com>
2019-09-03 19:02:38 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './app/layout/App';
+import * as serviceWorker from './serviceWorker';
+
+
+let render = () => {
+    ReactDOM.render(<App />, document.getElementById('root'));
+}
+
+
+if (module.hot) {
+    module.hot.accept('./app/layout/App', () => {
+        setTimeout(render)
+    })
+}
+
+render()
+
+serviceWorker.unregister();
